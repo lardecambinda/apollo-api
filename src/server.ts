7a3e402184dbd621bf6a1a1a2eecdcba96c19883
Julio Lima Costa Valladares <juliolima1225@gmail.com>
2023-02-07 02:22:06 -0300

    fix: env port to de server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import bodyParser from "body-parser";
 import express from "express";
 import router from "./routes/router";
 
+const port = process.env.PORT || 3001
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(router)
 
-app.listen(3001, () => console.log(`⚡️[server]: Server is running at https://localhost:3001`))
+app.listen(port, () => console.log(`⚡️[server]: Server is running at https://localhost:3001`))
