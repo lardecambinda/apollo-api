72230d50cdec05d3c8700acc6b08dd2c67ca15f0
Julio Lima Costa Valladares <juliolima1225@gmail.com>
2023-09-04 21:46:27 -0300

    feat/auth: protect store routes

--- src/routes/Comments/index.ts
+++ src/routes/Comments/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import CommentsController from "../../controller/CommentsController";
+import middleware from "../../middlewares/authMiddleware";
 
 const router = Router()
 
-router.post('/comment/create', CommentsController.store)
-router.post('/comment/list', CommentsController.store)
+router.post('/comment/create', middleware.auth, CommentsController.store)
+router.post('/comment/list', middleware.auth, CommentsController.store)
 
 export default router
--- src/routes/User/index.ts
+++ src/routes/User/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import RegisterController from "../../controller/UserController";
+import middleware from "../../middlewares/authMiddleware";
 
 const router = Router()
 
-router.post('/user/register', RegisterController.store)
+router.post('/user/register', middleware.auth,RegisterController.store)
 router.get('/user/list', RegisterController.findAll)
 router.get('/user/:id', RegisterController.findOne)
 
